0b11bbf3bc9ba04f0070a3af261387bad7d9651d
mingman <ingman.m@gmail.com>
2024-02-15 10:55:24 +0100

    added todo comment before break

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const url = "https://api.punkapi.com/v2/beers/random";
+let btnRandom = document.querySelector(".btn-random");
+let randomBeerWrapper = document.querySelector(".random-beer-wrapper");
+
 async function fetchBeer() {
   const response = await fetch(url);
   console.log(response);
   const data = await response.json();
   console.log(data);
-  const randomBeer = new Beer(data[0]);
+  let randomBeer = new Beer(data[0]);
   console.log(randomBeer);
+  btnRandom.addEventListener("click", function () {
+    // TODO: Click should set a new random beer and then send it to DOM
+    console.log("click");
+    // TODO: Move to separate class
+    let html = `<div class="card">
+            <img src="${randomBeer.image_url}" />
+            <h2>${randomBeer.name}</h2>
+            <p>See more</p>
+          </div>`;
+    randomBeerWrapper.innerHTML = "";
+    randomBeerWrapper.insertAdjacentHTML("afterbegin", html);
+  });
 }
-let btnRandom = document.querySelector(".btn-random");
-let randomBeer = document.querySelector(".random-beer-wrapper");
-btnRandom.addEventListener("click", function () {
-  console.log("click");
-  randomBeer.innerHTML = "hej";
-});
+
 fetchBeer();
