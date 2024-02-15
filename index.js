5dbe9f9bd1a3c409332103f271d1dce0feaa1495
mingman <ingman.m@gmail.com>
2024-02-15 10:24:36 +0100

    btn and event listner

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ async function fetchBeer() {
   console.log(response);
   const data = await response.json();
   console.log(data);
+  const randomBeer = new Beer(data[0]);
+  console.log(randomBeer);
 }
-
+let btnRandom = document.querySelector(".btn-random");
+btnRandom.addEventListener("click", function () {
+  console.log("click");
+});
 fetchBeer();
